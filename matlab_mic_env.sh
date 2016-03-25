9ab0d5c63efdcb2af29f02111e98742f0842209e
Yumeng Xu <yumengxu1994@icloud.com>
2016-03-25 21:48:12 +0800

    Copy the matlab lib needed to mic

---
+++ matlab_mic_env.sh
@@ -0,0 +1,8 @@
+export MKL_MIC_MAX_MEMORY=8G
+export MKL_MIC_ENABLE=1
+export BLAS_VERSION=/opt/intel/compilers_and_libraries_2016/linux/mkl/lib/intel64/libmkl_rt.so
+export LAPACK_VERSION=/opt/intel/compilers_and_libraries_2016/linux/mkl/lib/intel64/libmkl_rt.so
+export MKLROOT=/opt/intel/mkl
+export OFFLOAD_REPORT=2
+#export OMP_NUM_THREADS=
+#export MIC_OMP_NUM_THREADS=224
